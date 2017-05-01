4ece60b076c155efe221e8b8246439e31155acf1
Brian Fischer <starfox139@yahoo.com>
2017-05-01 01:35:22 -0400

    move camera with arrows and WASD

--- Assets/Scripts/Camera/DragCameraFollowPoint.cs
+++ Assets/Scripts/Camera/DragCameraFollowPoint.cs
@@ -12,28 +12,32 @@ public class DragCameraFollowPoint : MonoBehaviour {
 	[SerializeField] private Transform cameraTransform;
 
 	void Update () {
-		if (Input.GetMouseButtonUp (1) || Input.GetMouseButtonUp (2) || Input.GetMouseButtonDown (1) || Input.GetMouseButtonDown (2)) {
-			if (Input.GetMouseButton (1) || Input.GetMouseButton (2)) {
-				Cursor.visible = false;
-				Cursor.lockState = CursorLockMode.Locked;
-			}
-			else {
-				Cursor.lockState = CursorLockMode.None;
-				Cursor.visible = true;
-			}
+		if (Input.GetMouseButtonUp (1)) {
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+		else if (Input.GetMouseButtonDown (1)) {
+			Cursor.visible = false;
+			Cursor.lockState = CursorLockMode.Locked;
 		}
 
 		if (Input.GetMouseButton (1)) {
-			Vector3 tmp = transform.position;
+			/*Vector3 tmp = transform.position;
 			tmp += new Vector3 (cameraTransform.transform.forward.x, 0, cameraTransform.transform.forward.z).normalized * -Input.GetAxis ("Mouse Y") * followSpeed * Time.deltaTime;
 			tmp += cameraTransform.right * -Input.GetAxis ("Mouse X") * followSpeed * Time.deltaTime;
+			transform.position = tmp;*/
+			MoveCameraTarget (new Vector2 (-Input.GetAxis ("Mouse X"), -Input.GetAxis ("Mouse Y")));
+		}
+		Vector2 axes = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+		if (axes.magnitude > 0.01f) {
+			MoveCameraTarget (axes);
+		}
+	}
 
-			transform.position = tmp;
-		}
-		if (Input.GetMouseButton (2)) {
-			Vector3 euler = cameraTransform.eulerAngles;
-			euler.y += Input.GetAxis ("Mouse X") * followSpeed * 12 * Time.deltaTime;
-			cameraTransform.eulerAngles = euler;
-		}
+	private void MoveCameraTarget (Vector2 movement) {
+		Vector3 tmp = transform.position;
+		tmp += new Vector3 (cameraTransform.transform.forward.x, 0, cameraTransform.transform.forward.z).normalized * movement.y * followSpeed * Time.deltaTime;
+		tmp += cameraTransform.right * movement.x * followSpeed * Time.deltaTime;
+		transform.position = tmp;
 	}
 }
